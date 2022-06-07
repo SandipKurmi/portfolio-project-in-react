406f0d259c0666a1296c450b1c69e5575d9b520b
xsquare-110 <sandipkurmi0@gmail.com>
2022-06-07 16:29:50 +0530

    project setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css"
+import { useState } from 'react';
+
+function App() {
+
+
+
+
+  return (
+    <div className="App">
+      <h2>Portfolio Project</h2>
+    </div>
+  );
+}
+
+export default App;
